620bda891c520268d29785f261a0f3af4c163a0f
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2026-01-15 21:02:52 -0300

    feat: configuração inicial

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 import './bootstrap';
+import '../css/app.css';
+import { Link, Head } from '@inertiajs/vue3';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+        let page = pages[`./Pages/${name}.vue`];
+        //page.default.layout = page.default.layout || ((page) => h(Layout, { title: name }, () => h(page)));
+        return page;
+    },
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .component('Link', Link)
+            .component('Head', Head)
+            .mount(el);
+    }
+})
